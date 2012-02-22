285693a77079505c47288294063194b963969351
Fabien Basmaison <fabien@thenetcircle.com>
2012-02-22 21:57:01 +0800

    global and local estimations are now working + started working on displaying difference from ideal goal

--- some_folder/burndown_chart.php
+++ some_folder/burndown_chart.php
@@ -31,10 +31,11 @@
                             )
   );
 
-  $graphWidth     = 1000;
+  $graphWidth     = 2000;
+  $graphHeight    = 1000;
   $GraphMargin    = 50;
-  $withPoints     = $graphWidth / $sprint['points'];
-  $widthDay       = $graphWidth / $sprint['days'];
+  $unitPoint      = $graphHeight / $sprint['points'];
+  $unitDay        = $graphWidth / $sprint['days'];
   $coordsModifier = $sprint['points'] - $sprint['USPoints']; /* to adapt the number of US points to the scale of tasks points */
 ?>
 
@@ -50,37 +51,37 @@
 
   <g id="grid" transform="translate(<?= $GraphMargin. ',' .$GraphMargin; ?>)">
 <?php for ($i = 0; $i < $sprint['points']; $i += 10) : /* horizontal lines: points */ ?>
-    <line x1="<?= $GraphMargin; ?>" y1="<?= $graphWidth - $i * $withPoints; ?>" x2="<?= $graphWidth; ?>" y2="<?= $graphWidth + $GraphMargin - $i * $withPoints; ?>" stroke-dasharray="5,5" />
-    <text x="<?= $GraphMargin -.5; ?>" y="<?= $graphWidth - $i * $withPoints; ?>" text-anchor="end" dominant-baseline="middle"><?= $i; ?></text>
+    <line x1="0" y1="<?= $graphHeight - $i * $unitPoint; ?>" x2="<?= $graphWidth; ?>" y2="<?= $graphHeight - $i * $unitPoint; ?>" stroke-dasharray="5,5" />
+    <text x="-.5" y="<?= $graphHeight - $i * $unitPoint; ?>" text-anchor="end" dominant-baseline="middle"><?= $i; ?></text>
 <? endfor; ?>
 
-<?php for ($i = 0; $i < $sprint['days']; $i += 5) : /* vertical lines: days */ ?>
-    <line x1="<?= $graphWidth + $GraphMargin - $i * $widthDay; ?>" y1="<?= $GraphMargin; ?>" x2="<?= $graphWidth + $GraphMargin - $i * $widthDay; ?>" y2="<?= $graphWidth + $GraphMargin; ?>" />
+<?php for ($i = 0; $i < $sprint['days']; $i += 1) : /* vertical lines: days */ ?>
+    <line x1="<?= $graphWidth - $i * $unitDay; ?>" y1="0" x2="<?= $graphWidth - $i * $unitDay; ?>" y2="<?= $graphHeight; ?>" />
   <?php if($i != 0) : ?>
-    <text x="<?= $GraphMargin + $i * $widthDay; ?>" y="<?= $graphWidth + $GraphMargin + .5; ?>" text-anchor="middle" dominant-baseline="hanging">day <?= $i; ?></text>
+    <text x="<?= $i * $unitDay; ?>" y="<?= $graphHeight + .5; ?>" text-anchor="middle" dominant-baseline="hanging">day <?= $i; ?></text>
   <? endif; ?>
 <? endfor; ?>
 
-    <!-- ideal line -->
-    <line x1="<?= $GraphMargin; ?>" y1="<?= $GraphMargin; ?>" x2="<?= $GraphMargin + $graphWidth; ?>" y2="<?= $GraphMargin + $graphWidth; ?>" stroke="#eee" />
-
-<?php for ($i = 0; $i <= $sprint['days']; $i++) : /* ideal tasks remaining: dots */ ?>
-    <circle cx="<?= $GraphMargin + $i * $widthDay; ?>" cy="<?= $GraphMargin + $i * $widthDay; ?>" r="5" />
-<? endfor; ?>
+    <g id="ideal">
+      <line id="ideal_line" x1="0" y1="0" x2="<?= $graphWidth; ?>" y2="<?= $graphHeight; ?>" />
+      <?php for ($i = 0; $i <= $sprint['days']; $i++) : /* ideal tasks remaining: dots */ ?>
+      <circle cx="<?= $i * $unitDay; ?>" cy="<?= $i * $graphHeight / $sprint['days']; ?>" r="5" />
+      <? endfor; ?>
+    </g><!-- #grid -->
 
     <!-- frame -->
-    <rect x="<?= $GraphMargin; ?>" y="<?= $GraphMargin; ?>" width="<?= $graphWidth; ?>" height="<?= $graphWidth; ?>" fill="none" stroke="#000" />
-  </g><!-- /#grid -->
+    <rect x="0" y="0" width="<?= $graphWidth; ?>" height="<?= $graphHeight; ?>" fill="none" stroke="#000" />
+  </g><!-- #grid -->
 
   <!-- That's our goal -->
-  <circle cx="<?= 100 - $GraphMargin; ?>" cy="<?= 100 - $GraphMargin; ?>" r="5" fill="#090" stroke="#000" />
+  <circle cx="<?= $graphWidth; ?>" cy="<?= $graphHeight; ?>" r="5" fill="#090" stroke="#000" transform="translate(<?= $GraphMargin. ',' .$GraphMargin; ?>)" />
 
 
 
   <g id="legends" transform="translate(<?= $GraphMargin. ',' .$GraphMargin; ?>)">
-    <text x="<?= $GraphMargin - 3; ?>" y="<?= $GraphMargin - .5; ?>" fill="#900">Tasks</text>
-    <text x="<?= $GraphMargin - .5; ?>" y="<?= $GraphMargin - .5; ?>" fill="#900"><?= $sprint['points']; ?></text>
-    <text x="<?= $GraphMargin - 3; ?>" y="<?= $coordsModifier * $withPoints + $GraphMargin; ?>" fill="#069">User Story</text>
+    <text x="-3" y="-.5" fill="#900">Tasks</text>
+    <text x="-.5" y="-.5" fill="#900"><?= $sprint['points']; ?></text>
+    <text x="-3" y="<?= $coordsModifier * $unitPoint; ?>" fill="#069">User Story</text>
   </g><!-- /#legends -->
 
 
@@ -88,28 +89,28 @@
   <g id="chart-us" transform="translate(<?= $GraphMargin. ',' .$GraphMargin; ?>)">
 <?php for ($i = 0, $burnedPoints = 0; $i < count($sprint['dailyUSPoints']); $i++) : /* user stories */ ?>
   <?php
-    $previousX      = ($i-1) * $widthDay + $widthDay + $GraphMargin;
-    $previousY      = ($burnedPoints + $coordsModifier) * $withPoints + $GraphMargin;
+    $previousX      = ($i-1) * $unitDay + $unitDay;
+    $previousY      = ($burnedPoints + $coordsModifier) * $unitPoint;
     $burnedPoints   += $sprint['dailyUSPoints'][$i];
-    $x              = $i * $widthDay + $widthDay + $GraphMargin;
-    $y              = ($burnedPoints + $coordsModifier) * $withPoints + $GraphMargin;
+    $x              = $i * $unitDay + $unitDay;
+    $y              = ($burnedPoints + $coordsModifier) * $unitPoint;
   ?>
     <line x1="<?= $previousX; ?>" y1="<?= $previousY; ?>" x2="<?= $x; ?>" y2="<?= $y; ?>" />
 <? endfor; ?>
 <?php for ($i = 0, $burnedPoints = 0; $i < count($sprint['dailyUSPoints']); $i++) : /* user stories */ ?>
   <?php
-    $previousX      = ($i-1) * $widthDay + $widthDay + $GraphMargin;
-    $previousY      = ($burnedPoints + $coordsModifier) * $withPoints + $GraphMargin;
+    $previousX      = ($i-1) * $unitDay + $unitDay;
+    $previousY      = ($burnedPoints + $coordsModifier) * $unitPoint;
     $burnedPoints   += $sprint['dailyUSPoints'][$i];
-    $x              = $i * $widthDay + $widthDay + $GraphMargin;
-    $y              = ($burnedPoints + $coordsModifier) * $withPoints + $GraphMargin;
+    $x              = $i * $unitDay + $unitDay;
+    $y              = ($burnedPoints + $coordsModifier) * $unitPoint;
   ?>
     <circle cx="<?= $x; ?>" cy="<?= $y; ?>" r="5" />
     <text x="<?= $x + .5; ?>" y="<?= $y - .5; ?>"><?= $sprint['USPoints'] - $burnedPoints; ?></text>
 <? endfor; ?>
 
-    <circle cx="<?= $GraphMargin; ?>" cy="<?= $coordsModifier * $withPoints + $GraphMargin; ?>" r="5" />
-    <text x="<?= $GraphMargin + .5; ?>" y="<?= $coordsModifier * $withPoints + $GraphMargin -.5; ?>" fill="#069"><?= $sprint['USPoints']; ?></text>
+    <circle cx="0" cy="<?= $coordsModifier * $unitPoint; ?>" r="5" />
+    <text x=".5" y="<?= $coordsModifier * $unitPoint -.5; ?>" fill="#069"><?= $sprint['USPoints']; ?></text>
   </g><!-- /#chart-us -->
 
 
@@ -117,146 +118,131 @@
   <g id="chart-tasks" transform="translate(<?= $GraphMargin. ',' .$GraphMargin; ?>)">
 <?php for ($i = 0, $burnedPoints = 0; $i < count($sprint['dailyPoints']); $i++) : /* tasks */ ?>
   <?php
-    $previousX    = ($i-1) * $widthDay + $widthDay + $GraphMargin;
-    $previousY    = $burnedPoints * $withPoints + $GraphMargin;
+    $previousX    = ($i-1) * $unitDay + $unitDay;
+    $previousY    = $burnedPoints * $unitPoint;
     $burnedPoints += $sprint['dailyPoints'][$i];
-    $x            = $i * $widthDay + $widthDay + $GraphMargin;
-    $y            = $burnedPoints * $withPoints + $GraphMargin;
+    $x            = $i * $unitDay + $unitDay;
+    $y            = $burnedPoints * $unitPoint;
   ?>
     <line id="day<?= $i ?>" x1="<?= $previousX; ?>" y1="<?= $previousY; ?>" x2="<?= $x; ?>" y2="<?= $y; ?>" />
 
 <? endfor; ?>
 <?php for ($i = 0, $burnedPoints = 0; $i < count($sprint['dailyPoints']); $i++) : /* tasks */ ?>
   <?php
-    $previousX    = ($i-1) * $widthDay + $widthDay + $GraphMargin;
-    $previousY    = $burnedPoints * $withPoints + $GraphMargin;
+    $previousX    = ($i-1) * $unitDay + $unitDay;
+    $previousY    = $burnedPoints * $unitPoint;
     $burnedPoints += $sprint['dailyPoints'][$i];
-    $x            = $i * $widthDay + $widthDay + $GraphMargin;
-    $y            = $burnedPoints * $withPoints + $GraphMargin;
+    $x            = $i * $unitDay + $unitDay;
+    $y            = $burnedPoints * $unitPoint;
   ?>
     <circle id="point<?= $i ?>" cx="<?= $x; ?>" cy="<?= $y; ?>" r="5" />
     <text x="<?= $x - .5; ?>" y="<?= $y + .5; ?>"><?= $sprint['points'] - $burnedPoints; ?></text>
 
-
-
-
     <text class="pointsBurned" x="<?= $previousX + .5; ?>" y="<?= $previousY - .5; ?>" opacity="0">
-<!--
-      <animate attributeName="opacity" to="1" dur="0.25" begin="day<?= $i ?>.mouseover;point<?= $i-1 ?>.mouseover" fill="freeze" />
-      <animate attributeName="opacity" to="0" dur="0.25" begin="day<?= $i ?>.mouseout;point<?= $i-1 ?>.mouseout" fill="freeze" />
- -->
-
       <set attributeName="opacity" to="1" begin="day<?= $i ?>.mouseover;point<?= $i-1 ?>.mouseover" />
       <set attributeName="opacity" to="0" begin="day<?= $i ?>.mouseout;point<?= $i-1 ?>.mouseout" />
       <?= $sprint['dailyPoints'][$i]; ?>
     </text>
 <? endfor; ?>
 
-    <circle cx="<?= $GraphMargin; ?>" cy="<?= $GraphMargin; ?>" r="5" />
+    <circle cx="0" cy="0" r="5" />
   </g><!-- /#chart-tasks -->
 
 
 
 <?php
-
-  /* deviation of global project */
-  $ratioDeviation= ($y - $GraphMargin)/($x - $GraphMargin);
-  if($ratioDeviation <= 1) {
-    $deviationX  = 100 - $GraphMargin*2;
-    $deviationY  = $deviationX * $ratioDeviation + $GraphMargin;
-    $deviationX  = $deviationX + $GraphMargin;
-  }
-  else {
-    $deviationY  = 100 - $GraphMargin*2;
-    $deviationX  = $deviationY / $ratioDeviation + $GraphMargin;
-    $deviationY  = $deviationY + $GraphMargin;
-  }
-  $goalDeviation = abs(1 - $ratioDeviation);
-
-  /* projection for local estimation */
-  $ratioProjection  = ($y - $previousY)/($x - $previousX);
-  $threshold        = 100 - $GraphMargin;
-  $daysLeft         = $sprint['days'] - ($x-$GraphMargin)/$widthDay;
-  $projectionX      = $x;
-  $projectionY      = $daysLeft * ($y - $previousY) + $y;
-
-  if($projectionY > $threshold) {
-    $projectionY  = $threshold;
-  } else {
-    $projectionX  = $daysLeft * $widthDay + $x;
-    $projectionY  = $projectionY;
-  }
-  $goalProjection = abs(1 - $ratioProjection);
-
+  /* add 0 if value is only one digit (1 -> 01) */
   function digitNumber($value) {
     return strlen($value) < 2 ? '0'.$value : $value;
   }
-  $fillRed        = round($goalDeviation*100) == 0 ? '00' : 'ff';
-  $fillGreen      = round($goalDeviation*100) == 0 ? 187 : min( (1-$goalDeviation)*255, 210) - 75;
-  $fillGreen      = $fillGreen < 0 ? '00' : digitNumber(dechex($fillGreen));
-/*
-  $fillRed        = digitNumber(dechex( $goalDeviation*255 ));
-  $fillGreen      = digitNumber(dechex( min( (1-$goalDeviation)*255, 187) ));
-*/
-  $deviationColor = $fillRed.$fillGreen.'00';
-  $pointsLeft     = round($goalDeviation*100);
-  $pointsLeftProj = round($goalProjection*100);
-  if($pointsLeft == 0){
-    $pointsLeft   = $pointsLeftProj = 'Success!';
+
+  /* global estimation */
+  $globalSlope      = $y/$x;
+  $globalYIntercept = $y - $x * $globalSlope;
+  $globalX          = $graphWidth;
+  $globalY          = $globalX * $globalSlope + $globalYIntercept;
+  $globalDiff       = 100 - $globalY * 100 / $graphHeight;
+  if($globalY > $graphHeight) {
+    $globalY    = $graphHeight;
+    $globalX    = $globalY / $globalSlope - $globalYIntercept;
+    $globalDiff = 100 - $globalX * 100 / $graphwidth;
+  }
+
+
+  $globalRed    = round(100) == 0 ? '00' : 'ff';
+  $globalGreen  = round(100) == 0 ? 187 : min( (1)*255, 210) - 75;
+  $globalGreen  = $globalGreen < 0 ? '00' : digitNumber(dechex($globalGreen));
+  $globalColor  = $globalRed.$globalGreen.'00';
+
+  /* local estimation */
+  $localSlope       = ($y - $previousY) / ($x - $previousX);
+  $localYIntercept  = $y - $x * $localSlope;
+  $localX           = $graphWidth;
+  $localY           = $localSlope * $localX + $localYIntercept;
+  $localDiff        = 100 - $localY * 100 / $graphHeight;
+  if($localY > $graphHeight) {
+    $localY     = $graphHeight;
+    $localX     = ($localY - $localYIntercept) / $localSlope;
+    $localDiff  = 100 - $localX * 100 / $graphWidth;
+  }
+
+
+  /* points left */
+  if($globalDiff == 0){
+    $legendglobalDiff = $legendlocalDiff = 'Success!';
   }
   else{
-    $pointsLeft     = $ratioDeviation <= 1 ? $pointsLeft.'% left' : $pointsLeft.'% more';
-    $pointsLeftProj = $ratioProjection <= 1 ? $pointsLeftProj.'% left' : $pointsLeftProj.'% more';
+    $legendglobalDiff = $globalY < $graphHeight ? $globalDiff.'% left' : $globalDiff.'% more';
+    $legendlocalDiff  = $localY < $graphHeight ? $localDiff.'% left' : $localDiff.'% more';
   }
 
-  $blink          = round($goalDeviation*100) == 0 ? '' : '<animate attributeName="fill" values="#'.$deviationColor.';#f00;#'.$deviationColor.'" dur="1" repeatCount="indefinite" />';
+  $blink  = round(100) == 0 ? '' : '<animate attributeName="fill" values="#'.$deviationColor.';#f00;#'.$deviationColor.'" dur="1" repeatCount="indefinite" />';
 
 ?>
-  <g id="chart-deviation" fill="#<?= $deviationColor; ?>" stroke="#<?= $deviationColor; ?>" transform="translate(<?= $GraphMargin. ',' .$GraphMargin; ?>)">
+  <g id="chart-global" fill="#<?= $globalColor; ?>" stroke="#<?= $globalColor; ?>" transform="translate(<?= $GraphMargin. ',' .$GraphMargin; ?>)">
     <line
-      x1="<?= $GraphMargin; ?>" y1="<?= $GraphMargin; ?>"
-      x2="<?= $deviationX; ?>" y2="<?= $deviationY; ?>"
+      x1="0" y1="0"
+      x2="<?= $globalX; ?>" y2="<?= $globalY; ?>"
       stroke-dasharray="50,10,10,10" stroke-width="2" />
-    <circle cx="<?= $deviationX; ?>" cy="<?= $deviationY; ?>" r="<?= 5 * pow($goalDeviation+1, 3); ?>">
+    <circle cx="<?= $globalX; ?>" cy="<?= $globalY; ?>" r="5">
       <?= $blink; ?>
     </circle>
-    <text x="<?= $deviationX + .5; ?>" y="<?= $deviationY + 1.5 + pow($goalDeviation+.5, 3); ?>"><?= $pointsLeft; ?></text>
+    <text x="<?= $globalX + .5; ?>" y="<?= $globalY + 1.5; ?>"><?= $legendglobalDiff; ?></text>
   </g>
 
-  <g id="chart-projection" transform="translate(<?= $GraphMargin. ',' .$GraphMargin; ?>)">
+  <g id="chart-local" transform="translate(<?= $GraphMargin. ',' .$GraphMargin; ?>)">
     <line
       x1="<?= $x; ?>" y1="<?= $y; ?>"
-      x2="<?= $projectionX; ?>" y2="<?= $projectionY; ?>"
+      x2="<?= $localX; ?>" y2="<?= $localY; ?>"
       stroke-dasharray="50,10,10,10" stroke-width="2" />
-    <circle cx="<?= $projectionX; ?>" cy="<?= $projectionY; ?>" r="<?= 5 * pow($goalProjection+1, 3); ?>">
+    <circle cx="<?= $localX; ?>" cy="<?= $localY; ?>" r="<?= 5 * pow($goalProjection+1, 3); ?>">
     </circle>
-    <text x="<?= $projectionX + .5; ?>" y="<?= $projectionY + 1.5 + pow($goalProjection+.5, 3); ?>"><?= $pointsLeftProj; ?></text>
+    <text x="<?= $localX + .5; ?>" y="<?= $localY + 1.5 + pow($goalProjection+.5, 3); ?>"><?= $legendlocalDiff; ?></text>
   </g>
 
   <script type="text/javascript">
     console.log(''
+      +'\n GraphMargin:'+ <?= $GraphMargin; ?>
+      +'\n unitDay:'+ <?= $unitDay; ?>
+      +'\n ---'
       +'\n x:'+ <?= $x; ?>
       +'\n y:'+ <?= $y; ?>
       +'\n previousX:'+ <?= $previousX; ?>
       +'\n previousY:'+ <?= $previousY; ?>
-      +'\n GraphMargin:'+ <?= $GraphMargin; ?>
-      +'\n widthDay:'+ <?= $widthDay; ?>
-      +'\n daysLeft:'+ <?= $daysLeft; ?>
+      +'\n burnedPoints:'+ <?= $burnedPoints; ?>
+      +'\n globalDiff:'+ <?= $globalDiff; ?>
       +'\n ---'
-      +'\n ratioDeviation:'+ <?= $ratioDeviation; ?>
-      +'\n goalDeviation:'+ <?= $goalDeviation; ?>
-      +'\n deviationX:'+ <?= $deviationX; ?>
-      +'\n deviationY:'+ <?= $deviationY; ?>
+      +'\n globalSlope:'+ <?= $globalSlope; ?>
+      +'\n globalX:'+ <?= $globalX; ?>
+      +'\n globalY:'+ <?= $globalY; ?>
       +'\n ---'
-      +'\n $x - $previousX:'+ <?= $x - $previousX; ?>
-      +'\n $y - $previousY:'+ <?= $y - $previousY; ?>
-      +'\n ratioProjection:'+ <?= $ratioProjection; ?>
-      +'\n goalProjection:'+ <?= $goalProjection; ?>
-      +'\n projectionX:'+ <?= $projectionX; ?>
-      +'\n projectionY:'+ <?= $projectionY; ?>
+      +'\n globalRed: <?= $globalRed; ?>'
+      +'\n globalGreen: <?= $globalGreen; ?>'
+      +'\n globalColor: <?= $globalColor; ?>'
       +'\n ---'
-      +'\n fillRed: <?= $fillRed; ?>'
-      +'\n fillGreen: <?= $fillGreen; ?>'
+      +'\n localSlope:'+ <?= $localSlope; ?>
+      +'\n localX:'+ <?= $localX; ?>
+      +'\n localY:'+ <?= $localY; ?>
     );
   </script>
 </svg>
