2ac9537c66848e4b1d073e078fb06ab9f5639171
Fabien Basmaison <fabien@thenetcircle.com>
2012-03-14 15:34:46 +0800

    added index file to avoid playing with SVG headers (for embbedding it or not)

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>burndown chart in SVG</title>
+
+  <link rel="stylesheet" href="common.css" />
+</head>
+
+<body>
+
+<?php include('burndown_chart.php'); ?>
+
+</body>
+</html>
